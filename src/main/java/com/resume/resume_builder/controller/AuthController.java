406743834de34258d277d1fbb980aedcb0ea2504
arun1111j <justarun0210@gmail.com>
2025-04-25 13:32:44 +0530

    Ready for Render Deployment

--- src/main/java/com/resume/resume_builder/controller/AuthController.java
+++ src/main/java/com/resume/resume_builder/controller/AuthController.java
@@ -4,8 +4,6 @@ import java.util.Map;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/resume/resume_builder/controller/UserController.java
+++ src/main/java/com/resume/resume_builder/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.resume.resume_builder.controller;
 
 import com.resume.resume_builder.util.AuthUtils;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
