c3f06fcf2311b5790c1905db795c61451f423334
arun1111j <justarun0210@gmail.com>
2025-04-25 13:36:11 +0530

    Ready for Render Deployment

--- src/main/java/com/resume/resume_builder/controller/UserController.java
+++ src/main/java/com/resume/resume_builder/controller/UserController.java
@@ -4,13 +4,14 @@ import com.resume.resume_builder.util.AuthUtils;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@Controller
 public class UserController {
 
     @GetMapping("/user")
